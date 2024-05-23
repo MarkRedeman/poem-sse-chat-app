fe756c5db0cfad403ee09359ac7118d7bf0cc7d7
Mark Redeman <markredeman@gmail.com>
2024-05-23 23:25:42 +0200

    Initialize shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,23 @@
+import path from "path";
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+    plugins: [
+        remix({
+            ssr: false,
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true,
+            },
+        }),
+        tsconfigPaths(),
+    ],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./app"),
+        },
+    },
 });
