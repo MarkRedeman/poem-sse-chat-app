f7da5d5bae0fea932c2a64d61298de7196617742
Mark Redeman <markredeman@gmail.com>
2024-05-28 23:20:05 +0200

    Login form

--- client/app/components/login-form.tsx
+++ client/app/components/login-form.tsx
@@ -9,23 +9,45 @@ import {
 } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
+import { useFetcher } from "@remix-run/react";
 
 export function LoginForm() {
+  const fetcher = useFetcher();
+
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>Choose your username</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="username">Username</Label>
-          <Input id="username" type="text" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Sign in</Button>
-      </CardFooter>
-    </Card>
+    <fetcher.Form method="POST" action="/">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>Choose your username</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              defaultValue={"Mark"}
+              id="username"
+              name="username"
+              type="text"
+              required
+              minLength={2}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Sign in</Button>
+        </CardFooter>
+      </Card>
+    </fetcher.Form>
+  );
+}
+
+export function LogoutForm() {
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="DELETE">
+      <Button className="w-full">Logout</Button>
+    </fetcher.Form>
   );
 }
--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,5 +1,58 @@
 import type { MetaFunction } from "@remix-run/node";
-import { LoginForm } from "~/components/login-form";
+import {
+  ClientActionFunctionArgs,
+  Outlet,
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+  redirect,
+} from "@remix-run/react";
+import { LoginForm, LogoutForm } from "~/components/login-form";
+
+export const clientLoader = async () => {
+  const response = await fetch("http://localhost:3000/api/session", {
+    credentials: "include",
+  });
+
+  if (response.status === 401) {
+    throw new Response("Unauthorized", { status: 401 });
+  }
+
+  return await response.json();
+};
+
+export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
+  if (request.method === "DELETE") {
+    await fetch("http://localhost:3000/api/session", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    return redirect("/");
+  }
+
+  if (request.method === "POST") {
+    const form = await request.formData();
+    const username = form.get("username");
+
+    await fetch("http://localhost:3000/api/session", {
+      method: "POST",
+      body: JSON.stringify({ username }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      credentials: "include",
+    });
+
+    return redirect("/");
+  }
+
+  throw new Response("Not found", { status: 404 });
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,10 +61,50 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.log("there is an error", { error });
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 401) {
+      return (
+        <div className="w-full h-screen flex items-center justify-center">
+          <LoginForm />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
+}
+
 export default function Index() {
+  const data = useLoaderData();
+
+  console.log("Data", { data });
+
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <LoginForm />
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
+      <h1>Logged in</h1>
+      <LogoutForm />
     </div>
   );
 }
