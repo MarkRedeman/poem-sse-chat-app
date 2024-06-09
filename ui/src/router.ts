5be3e09edf3bf4eff0e71c74826650565629e935
Mark Redeman <markredeman@gmail.com>
2024-06-09 17:53:37 +0200

    Extend lazy loader to build actions

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -1,5 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 import {
+  ActionFunction,
   LazyRouteFunction,
   LoaderFunction,
   RouteObject,
@@ -10,26 +11,26 @@ export type AppContext = { queryClient: QueryClient };
 export function lazy<
   R extends RouteObject & {
     buildLoader?: (context: AppContext) => LoaderFunction;
+    buildAction?: (context: AppContext) => ActionFunction;
   },
 >(
   importedPromise: Promise<R>,
-  { queryClient }: AppContext
+  ctx: AppContext
 ): LazyRouteFunction<RouteObject> | undefined {
   // @ts-expect-error I'm not sure how  to fix this yet
   return async () => {
     const imported = await importedPromise;
 
-    if (imported === undefined) {
-      return undefined;
-    }
+    const loader =
+      imported.buildLoader !== undefined
+        ? imported.buildLoader(ctx)
+        : imported.loader;
 
-    if (imported.buildLoader) {
-      return {
-        ...imported,
-        loader: imported.buildLoader({ queryClient }),
-      };
-    }
+    const action =
+      imported.buildAction !== undefined
+        ? imported.buildAction(ctx)
+        : imported.action;
 
-    return imported;
+    return { ...imported, loader, action };
   };
 }
