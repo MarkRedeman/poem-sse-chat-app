a079e903ecbe9ae5d8778754ff37f5a110d33888
Mark Redeman <markredeman@gmail.com>
2024-07-24 22:07:01 +0200

    Add debounce to live loader

--- ui/src/lib/use-live-loader.ts
+++ ui/src/lib/use-live-loader.ts
@@ -1,16 +1,32 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { useEffect, useRef } from "react";
 import { useRevalidator } from "react-router-dom";
 
+import { sessionQueryOptions } from "./session";
 import { useEventSource } from "./use-event-source";
 import { useLoaderData } from "./use-loader-data";
 
 const wait = (n: number) => new Promise((resolve) => setTimeout(resolve, n));
 
+function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
+  func: F,
+  waitFor: number
+) {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  const debounced = (...args: Parameters<F>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced;
+}
+
 export function useLiveLoader<T>(
   eventSourceUrl: string,
   dataResolver?: (revalidate: () => void) => void
 ) {
+  const sessionQuery = useSuspenseQuery(sessionQueryOptions());
   //return useLoaderData<T>();
   const queryClient = useQueryClient();
   const data = useEventSource(eventSourceUrl);
@@ -21,14 +37,33 @@ export function useLiveLoader<T>(
     resolver.current = dataResolver;
   }, [dataResolver]);
 
+  const debouncedRefresh = useRef(
+    debounce(() => {
+      if (resolver.current) {
+        resolver.current(revalidate);
+      } else {
+        queryClient.refetchQueries();
+      }
+    }, 100)
+  );
+
   useEffect(() => {
     try {
       const event = JSON.parse(data ?? "{}");
       console.log("Got event", data, event);
 
+      // data: {"payload":{"type":"UserLoggedOut","username":"Mark - firefox"},"type":"UserLoggedOut"}
+
       if (event?.type === "UserLoggedOut") {
-        queryClient.cancelQueries();
-        queryClient.clear();
+        const username = event.payload.username;
+
+        console.log({ username });
+
+        if (username === sessionQuery.data.username) {
+          console.log("logging out", { username, sessionQuery });
+          queryClient.cancelQueries();
+          queryClient.clear();
+        }
         return;
       }
     } catch (e) {
@@ -38,23 +73,7 @@ export function useLiveLoader<T>(
     console.log("Received the event..");
 
     // Add a bit of throttling so that we can focus on optimistic updates next..
-    wait(10).then(() => {
-      if (resolver.current) {
-        resolver.current(revalidate);
-      } else {
-        queryClient.refetchQueries();
-        // queryClient
-        //   .invalidateQueries({
-        //     queryKey: ["rooms"],
-        //     exact: false,
-        //     stale: true,
-        //     refetchType: "all",
-        //   })
-        //   .then(() => {
-        //     revalidate();
-        //   });
-      }
-    });
+    debouncedRefresh.current();
   }, [data, revalidate]);
 
   return useLoaderData<T>();
